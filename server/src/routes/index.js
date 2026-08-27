fb1c78ae61bd5cb689d4727b4a67bb7a35b3ec10
John Dave Villasan <dave.uc.main.it@gmail.com>
2026-08-27 18:04:22 +0800

    Add exercise catalogue endpoints

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 'use strict';
 const express = require('express');
 const buildHealthRouter = require('./health');
+const buildExercisesRouter = require('./exercises');
 
 module.exports = function buildRoutes(deps = {}) {
   const router = express.Router();
   router.use('/health', buildHealthRouter(deps));
+  router.use('/exercises', buildExercisesRouter(deps));
   if (deps.extraRouter) router.use(deps.extraRouter);
   return router;
 };
