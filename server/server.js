998f4fc8f29a8f06c5b26d1f4853c9a7309df91c
John Dave Villasan <dave.uc.main.it@gmail.com>
2026-09-04 16:41:52 +0800

    Wire the mail service into the running server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { createPool } = require('./src/db/pool');
 const { createMlService } = require('./src/services/ml');
 const { createStorage } = require('./src/services/storage');
 const { createGoogleVerifier } = require('./src/services/google');
+const { createMailService } = require('./src/services/mail');
 const { createApp } = require('./src/app');
 
 const config = load();
@@ -13,8 +14,12 @@ const pool = createPool(config.db);
 const ml = createMlService(config.ml);
 const storage = createStorage(config.storage);
 const google = createGoogleVerifier(config.google);
+const mail = createMailService(config.mail, logger);
 
-const app = createApp({ config, logger, pool, ml, storage, jwt: config.jwt, google });
+const app = createApp({
+  config, logger, pool, ml, storage, jwt: config.jwt, google,
+  mail, publicBaseUrl: config.publicBaseUrl,
+});
 
 const server = app.listen(config.port, () => {
   logger.info(`FitSync API listening on port ${config.port} (${config.env})`);
