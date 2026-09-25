9bb1ca103e38ae1d328a0fb5973de4f2453c18b9
John Dave Villasan <dave.uc.main.it@gmail.com>
2026-09-25 22:29:28 +0800

    Load a smaller time zone database
    latest_all.dart carries every zone the timezone package knows; switch
    to latest_10y.dart, which is enough (Asia/Manila still resolves) and
    smaller to ship.

--- client/lib/features/reminders/data/local_reminder_scheduler.dart
+++ client/lib/features/reminders/data/local_reminder_scheduler.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/data/latest_all.dart' as tz_data;
+import 'package:timezone/data/latest_10y.dart' as tz_data;
 import 'package:timezone/timezone.dart' as tz;
 
 import '../domain/reminders.dart';
